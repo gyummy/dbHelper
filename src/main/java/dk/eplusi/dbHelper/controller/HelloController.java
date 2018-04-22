1da6eab64e891b15fad94cacdad6dd48d4f14501
gyummy <gyummy0917@naver.com>
2018-04-23 01:03:55 +0900

    UPDATE: changed server template from velocity to handlebars

--- src/main/java/dk/eplusi/dbHelper/controller/HelloController.java
+++ src/main/java/dk/eplusi/dbHelper/controller/HelloController.java
@@ -2,12 +2,13 @@ package dk.eplusi.dbHelper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String index(Model model) {
         model.addAttribute("name", "GYUMMY");
         return "hello";
